e87036e4a135b70513d9899a2f34bcb57ec3af64
shoogeya <eng.shoog93@gmail.com>
2023-07-23 11:11:55 +0300

    Add bottom nav bar

---
+++ lib/app/modules/bottomNavigation/main_navigation_Layout.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class MainNavigationLayout extends StatelessWidget {
+  const MainNavigationLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
